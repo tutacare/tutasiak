b3a36b04e309738a61ba4f2503532e2781985aea
Irfan Mahfudz Guntur <tutacare@gmail.com>
2015-07-13 22:25:13 +0800

    fix student ++1

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+  public function faculty()
+  {
+    return $this->belongsTo('App\Faculty', 'faculty_code', 'faculty_code');
+  }
+
+  public function study()
+  {
+    return $this->belongsTo('App\Study');
+  }
+
+  public function programStudy()
+  {
+    return $this->belongsTo('App\ProgramStudy', 'program_study_id', 'id');
+  }
+
+  public function academicRegistration()
+  {
+    return $this->belongsTo('App\AcademicRegistration', 'academic_registration_id', 'id');
+  }
+
+  public function religion()
+  {
+    return $this->belongsTo('App\Religion');
+  }
 }
--- resources/views/dashboard/admin/student/index.blade.php
+++ resources/views/dashboard/admin/student/index.blade.php
@@ -16,35 +16,35 @@
 <table class="table table-striped table-bordered">
     <thead>
         <tr>
-            <td><small>NIM</small></td>
-            <td><small>Name</small></td>
-            <td><small>classroom</small></td>
-            <td><small>faculty_code</small></td>
-            <td><small>study_id</small></td>
-            <td><small>program_study_id</small></td>
-            <td><small>academic_registration_id</small></td>
-            <td><small>email</small></td>
-            <td><small>religion_id</small></td>
-            <td><small>address</small></td>
-            <td><small>phone</small></td>
-            <td><small>Active</small></td>
+            <td>NIM</td>
+            <td>Name</td>
+            <td>Class Room</td>
+            <td>Faculty</td>
+            <td>Study</td>
+            <td>Program Study</td>
+            <td>Academic Year</td>
+            <td>Email</td>
+            <td>Religion</td>
+            <td>Address</td>
+            <td>Phone</td>
+            <td>Active</td>
             <td colspan="2">&nbsp;</td>
         </tr>
     </thead>
     <tbody>
     @foreach($student as $value)
         <tr>
-            <td><small>{{ $value->nim }}</small></td>
-            <td><small>{{ $value->name }}</small></td>
-            <td><small>{{ $value->classroom }}</small></td>
-            <td><small>{{ $value->faculty_code }}</small></td>
-            <td><small>{{ $value->study_id }}</small></td>
-            <td><small>{{ $value->program_study_id }}</small></td>
-            <td><small>{{ $value->academic_registration_id }}</small></td>
-            <td><small>{{ $value->email }}</small></td>
-            <td><small>{{ $value->religion_id }}</small></td>
-            <td><small>{{ $value->address }}</small></td>
-            <td><small>{{ $value->phone }}</small></td>
+            <td>{{ $value->nim }}</td>
+            <td>{{ $value->name }}</td>
+            <td>{{ $value->classroom }}</td>
+            <td>{{ $value->faculty->faculty_name }}</td>
+            <td>{{ $value->study->study_name }}</td>
+            <td>{{ $value->programStudy->name }}</td>
+            <td>{{ $value->academicRegistration->academic_year }}</td>
+            <td>{{ $value->email }}</td>
+            <td>{{ $value->religion->name }}</td>
+            <td>{{ $value->address }}</td>
+            <td>{{ $value->phone }}</td>
             <td>{{ $value->active }}</td>
             <td>
                 <a class="btn btn-xs btn-info" href="{{ URL::to('dashboard/admin/students/' . $value->id . '/edit') }}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
